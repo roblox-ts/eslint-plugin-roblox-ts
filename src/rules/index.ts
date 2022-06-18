667dadbd39d7c38ae40f55734131e393e8d2bed8
Dion <39664950+Dionysusnu@users.noreply.github.com>
2022-06-18 22:06:41 +0200

    Update index.ts

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,6 +1,5 @@
 export * from "./lua-truthiness";
 export * from "./misleadingLuatupleChecks";
-export * from "./module";
 export * from "./noAny";
 export * from "./noDelete";
 export * from "./noEnumMerging";
