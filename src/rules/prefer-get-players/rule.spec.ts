b3be569aed7605b6c00972001121e1c13a781e7d
christopher-buss <christopher.buss+github@protonmail.com>
2025-05-13 23:39:15 +0100

    feat: add `prefer-get-players` rule

---
+++ src/rules/prefer-get-players/rule.spec.ts
@@ -0,0 +1,88 @@
+import type { InvalidTestCase, ValidTestCase } from "eslint-vitest-rule-tester";
+
+import { run } from "../test";
+import { preferGetPlayers, RULE_NAME } from "./rule";
+
+const messageId = "get-players-children-violation";
+
+type RuleOptions = readonly [{ validateType: boolean }];
+
+const valid: Array<ValidTestCase<RuleOptions>> = [
+	"const players = Players.GetPlayers()",
+	"const children = workspace.GetChildren()",
+	'Players.FindFirstChild("SomePlayer")',
+	"print(players.GetChildren())",
+	'print(game.GetService("Players").GetChildren())',
+];
+
+const validTypeChecked: Array<ValidTestCase<RuleOptions>> = [
+	'const y = game.GetService("Players"); for (const player of y.GetPlayers()) {}',
+	'const Players = Instance.new("Part"); print(Players.GetChildren())',
+	'function x(players: Omit<Players, "GetPlayers">) { for (const player of players.GetChildren()) {} }',
+];
+
+const invalid: Array<InvalidTestCase<RuleOptions>> = [
+	{
+		code: "for (const player of Players.GetChildren()) {}",
+		errors: [{ messageId }],
+		output: "for (const player of Players.GetPlayers()) {}",
+	},
+	{
+		code: "const allPlayers = Players.GetChildren()",
+		errors: [{ messageId }],
+		output: "const allPlayers = Players.GetPlayers()",
+	},
+	{
+		code: "function getPlayers() { return Players.GetChildren() }",
+		errors: [{ messageId }],
+		output: "function getPlayers() { return Players.GetPlayers() }",
+	},
+];
+
+const invalidTypeChecked: Array<InvalidTestCase<RuleOptions>> = [
+	{
+		code: 'const y = game.GetService("Players"); for (const player of y.GetChildren()) {}',
+		errors: [{ messageId }],
+		output: 'const y = game.GetService("Players"); for (const player of y.GetPlayers()) {}',
+	},
+	{
+		code: 'print(game.GetService("Players").GetChildren())',
+		errors: [{ messageId }],
+		output: 'print(game.GetService("Players").GetPlayers())',
+	},
+	{
+		code: "function x(alias: Players) { for (const player of alias.GetChildren()) {} }",
+		errors: [{ messageId }],
+		output: "function x(alias: Players) { for (const player of alias.GetPlayers()) {} }",
+	},
+	{
+		code: "interface G extends Players {}; let x: G; for (const player of x.GetChildren()) {}",
+		errors: [{ messageId }],
+		output: "interface G extends Players {}; let x: G; for (const player of x.GetPlayers()) {}",
+	},
+];
+
+run({
+	invalid: [
+		...invalid,
+		...invalidTypeChecked.map(test => {
+			return {
+				...(typeof test === "object" ? test : undefined),
+				code: typeof test === "string" ? test : test.code,
+				options: [{ validateType: true }],
+			};
+		}),
+	],
+	name: RULE_NAME,
+	rule: preferGetPlayers,
+	valid: [
+		...valid,
+		...validTypeChecked.map(test => {
+			return {
+				...(typeof test === "object" ? test : undefined),
+				code: typeof test === "string" ? test : test.code,
+				options: [{ validateType: true }],
+			};
+		}),
+	],
+});
---
+++ src/rules/prefer-get-players/rule.ts
@@ -0,0 +1,128 @@
+import { getConstrainedTypeAtLocation, isBuiltinSymbolLike } from "@typescript-eslint/type-utils";
+import type { TSESLint, TSESTree } from "@typescript-eslint/utils";
+import { AST_NODE_TYPES } from "@typescript-eslint/utils";
+import { getParserServices } from "@typescript-eslint/utils/eslint-utils";
+
+import { createEslintRule } from "../../util";
+
+export const RULE_NAME = "prefer-get-players";
+
+const MESSAGE_ID = "get-players-children-violation";
+
+const messages = {
+	[MESSAGE_ID]:
+		"Use Players.GetPlayers() instead of Players.GetChildren() for more accurate types.",
+};
+
+type Context = Readonly<TSESLint.RuleContext<MessageIds, RuleOptions>>;
+type MessageIds = typeof MESSAGE_ID;
+type RuleOptions = [{ validateType: boolean }];
+
+function check(
+	context: Context,
+	node: TSESTree.CallExpression,
+	callee: TSESTree.MemberExpression,
+): void {
+	context.report({
+		fix: fixer => fixer.replaceText(callee.property, "GetPlayers"),
+		messageId: MESSAGE_ID,
+		node,
+	});
+}
+
+function create(
+	context: Context,
+	[{ validateType }]: Readonly<RuleOptions>,
+): TSESLint.RuleListener {
+	return {
+		CallExpression(node: TSESTree.CallExpression) {
+			const { callee } = node;
+			if (
+				callee.type !== AST_NODE_TYPES.MemberExpression ||
+				callee.property.type !== AST_NODE_TYPES.Identifier ||
+				callee.property.name !== "GetChildren"
+			) {
+				return;
+			}
+
+			if (validateType) {
+				isPlayersCallExpressionType(context, node, callee);
+			} else {
+				isPlayersCallExpression(context, node, callee);
+			}
+		},
+	};
+}
+
+function isPlayersCallExpression(
+	context: Context,
+	node: TSESTree.CallExpression,
+	callee: TSESTree.MemberExpression,
+): void {
+	if (callee.object.type !== AST_NODE_TYPES.Identifier || callee.object.name !== "Players") {
+		return;
+	}
+
+	check(context, node, callee);
+}
+
+function isPlayersCallExpressionType(
+	context: Context,
+	node: TSESTree.CallExpression,
+	callee: TSESTree.MemberExpression,
+): void {
+	const parserServices = getParserServices(context);
+	const type = getConstrainedTypeAtLocation(parserServices, callee.object);
+
+	const isPlayersType = isBuiltinSymbolLike(parserServices.program, type, ["Players"]);
+	if (!isPlayersType) {
+		return;
+	}
+
+	const hasGetPlayersProperty = type.getProperty("GetPlayers");
+	if (!hasGetPlayersProperty) {
+		return;
+	}
+
+	check(context, node, callee);
+}
+
+export const preferGetPlayers = createEslintRule({
+	create,
+	defaultOptions: [
+		{
+			validateType: false,
+		},
+	],
+	meta: {
+		defaultOptions: [
+			{
+				validateType: false,
+			},
+		],
+		docs: {
+			description:
+				"Enforces the use of Players.GetPlayers() instead of Players.GetChildren()",
+			recommended: false,
+			requiresTypeChecking: false,
+		},
+		fixable: "code",
+		hasSuggestions: false,
+		messages,
+		schema: [
+			{
+				properties: {
+					validateType: {
+						default: false,
+						description:
+							"Enable or disable type validation. Useful if your Players variable has an alias.",
+						type: "boolean",
+					},
+				},
+				type: "object",
+			},
+		],
+		type: "problem",
+	},
+	name: RULE_NAME,
+});
