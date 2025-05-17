642b4bf40455ffc7a9ae46710355e66c411e2437
christopher-buss <christopher.buss+github@protonmail.com>
2025-05-17 17:49:47 +0100

    feat(no-user-defined-lua-tuple): add autofix to rule

--- src/rules/no-user-defined-lua-tuple/rule.spec.ts
+++ src/rules/no-user-defined-lua-tuple/rule.spec.ts
@@ -15,22 +15,27 @@ const invalid: Array<InvalidTestCase> = [
 	{
 		code: "type X = LuaTuple<[string, number]>;",
 		errors: [{ messageId }],
+		output: "type X = [string, number];",
 	},
 	{
 		code: "interface LuaTuple<T> { value: T }",
 		errors: [{ messageId }],
+		output: null,
 	},
 	{
 		code: "type LuaTuple<T> = T[];",
 		errors: [{ messageId }],
+		output: null,
 	},
 	{
 		code: "function foo(): LuaTuple<[number, string]> {}",
 		errors: [{ messageId }],
+		output: "function foo(): [number, string] {}",
 	},
 	{
 		code: "let t: LuaTuple<[boolean]>;",
 		errors: [{ messageId }],
+		output: "let t: [boolean];",
 	},
 	{
 		code: unindent`
@@ -39,6 +44,57 @@ const invalid: Array<InvalidTestCase> = [
 			}
 		`,
 		errors: [{ messageId }, { messageId }],
+		output: unindent`
+			function bar(): [number, string] {
+				return [42, "hello"];
+			}
+		`,
+	},
+	{
+		code: "class C { prop: LuaTuple<[number]>; }",
+		errors: [{ messageId }],
+		output: "class C { prop: [number]; }",
+	},
+	{
+		code: "function fn(param: LuaTuple<[string]>): void {}",
+		errors: [{ messageId }],
+		output: "function fn(param: [string]): void {}",
+	},
+	{
+		code: "let x: LuaTuple;",
+		errors: [{ messageId }],
+	},
+	{
+		code: "type ComplexArray = Array<LuaTuple<[string]>>;",
+		errors: [{ messageId }],
+		output: "type ComplexArray = Array<[string]>;",
+	},
+	{
+		code: "type UnionType = LuaTuple<[number]> | string;",
+		errors: [{ messageId }],
+		output: "type UnionType = [number] | string;",
+	},
+	{
+		code: "type IntersectionType = LuaTuple<[boolean]> & { extra: string };",
+		errors: [{ messageId }],
+		output: "type IntersectionType = [boolean] & { extra: string };",
+	},
+	{
+		code: unindent`
+			type GenericContainer<T> = { item: T };
+			let val: GenericContainer<LuaTuple<[number, string]>>;
+		`,
+		errors: [{ messageId }],
+		output: unindent`
+			type GenericContainer<T> = { item: T };
+			let val: GenericContainer<[number, string]>;
+		`,
+	},
+	{
+		code: "type Y = LuaTuple<[string, number]>;",
+		errors: [{ messageId }],
+		options: [{ fixToTuple: false }],
+		output: null,
 	},
 ];
 
--- src/rules/no-user-defined-lua-tuple/rule.ts
+++ src/rules/no-user-defined-lua-tuple/rule.ts
@@ -1,4 +1,4 @@
-import type { TSESLint, TSESTree } from "@typescript-eslint/utils";
+import { type TSESLint, TSESTree } from "@typescript-eslint/utils";
 
 import { createEslintRule } from "../../util";
 
@@ -10,7 +10,11 @@ const messages = {
 	[MESSAGE_ID]: "Disallow usage of the LuaTuple type keyword.",
 };
 
-function create(context: Readonly<TSESLint.RuleContext<string, []>>): TSESLint.RuleListener {
+type Context = Readonly<TSESLint.RuleContext<typeof MESSAGE_ID, [{ fixToTuple: boolean }]>>;
+
+function create(context: Context): TSESLint.RuleListener {
+	const { fixToTuple } = context.options[0];
+
 	return {
 		'TSInterfaceDeclaration[id.name="LuaTuple"]': (node: TSESTree.TSInterfaceDeclaration) => {
 			context.report({
@@ -28,6 +32,7 @@ function create(context: Readonly<TSESLint.RuleContext<string, []>>): TSESLint.R
 			node: TSESTree.TSTypeReference,
 		) => {
 			context.report({
+				fix: fixToTuple ? fixer => fixLuaTuple(node, context, fixer) : null,
 				messageId: MESSAGE_ID,
 				node: node.typeName,
 			});
@@ -35,19 +40,68 @@ function create(context: Readonly<TSESLint.RuleContext<string, []>>): TSESLint.R
 	};
 }
 
+function fixLuaTuple(
+	node: TSESTree.TSTypeReference,
+	context: Context,
+	fixer: TSESLint.RuleFixer,
+): null | TSESLint.RuleFix {
+	const typeArgumentNode = node.typeArguments?.params[0];
+	if (!typeArgumentNode) {
+		return null;
+	}
+
+	const typeArgumentText = context.sourceCode.getText(typeArgumentNode);
+	const { parent } = node;
+
+	if (parent.type === TSESTree.AST_NODE_TYPES.TSAsExpression && parent.typeAnnotation === node) {
+		const asExpression = parent;
+		// If the parent is an AS expression and this node is the type annotation,
+		// remove the 'as LuaTuple<...>' part by replacing the range from
+		// the end of the expression to the end of the asExpression.
+		return fixer.replaceTextRange(
+			[asExpression.expression.range[1], asExpression.range[1]],
+			"",
+		);
+	}
+
+	// Otherwise, just replace LuaTuple<...> with ... (the type argument itself)
+	return fixer.replaceText(node, typeArgumentText);
+}
+
 export const noUserDefinedLuaTuple = createEslintRule({
 	create,
-	defaultOptions: [],
+	defaultOptions: [
+		{
+			fixToTuple: true,
+		},
+	],
 	meta: {
+		defaultOptions: [
+			{
+				fixToTuple: true,
+			},
+		],
 		docs: {
 			description: "Disallow usage of the LuaTuple type keyword",
-			recommended: false,
+			recommended: true,
 			requiresTypeChecking: false,
 		},
-		fixable: undefined,
+		fixable: "code",
 		hasSuggestions: false,
 		messages,
-		schema: [],
+		schema: [
+			{
+				additionalProperties: false,
+				properties: {
+					fixToTuple: {
+						description:
+							"Whether to enable auto-fixing in which the `LuaTuple` type is converted to a native TypeScript tuple type.",
+						type: "boolean",
+					},
+				},
+				type: "object",
+			},
+		],
 		type: "problem",
 	},
 	name: RULE_NAME,
